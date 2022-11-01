1ef6d199fa57bd1688cc1a068e8effc2ff1b0be8
LeeHalim <leehalim09@gmail.com>
2022-11-02 00:40:23 +0900

    added bootstrapVue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainHeaderVue />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainHeaderVue from "@/components/MainHeader.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    MainHeaderVue,
+  },
+};
 </script>
