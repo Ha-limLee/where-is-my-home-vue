feebfbde7683d1b4040e33c3ab363b79d91fbe46
LeeHalim <leehalim09@gmail.com>
2022-11-03 13:47:13 +0900

    impled MyPage

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,7 +4,6 @@
     <b-container class="mt-4">
       <b-row align-h="center">
         <h3>내 정보</h3>
-        <h3>{{ form.address }}</h3>
       </b-row>
       <b-form @submit="onSubmit" @reset="onReset" v-if="show">
         <b-form-group id="input-group-id" label="아이디" label-for="input-id" description="">
@@ -14,6 +13,7 @@
             type="text"
             placeholder="아이디를 입력해주세요"
             required
+            disabled
           ></b-form-input>
         </b-form-group>
 
@@ -23,16 +23,6 @@
             v-model="form.userName"
             type="text"
             placeholder="이름을 입력해주세요"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-pwd" label="비밀번호" label-for="input-pwd">
-          <b-form-input
-            id="input-pwd"
-            v-model="form.userPassword"
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
             required
           ></b-form-input>
         </b-form-group>
@@ -62,7 +52,7 @@
           ></b-form-input>
         </b-form-group>
 
-        <b-button type="submit" class="mr-2" variant="primary">회원가입</b-button>
+        <b-button type="submit" class="mr-2" variant="primary">수정</b-button>
         <b-button type="reset" variant="danger">초기화</b-button>
       </b-form>
       <b-card class="mt-3" header="Form Data Result">
@@ -82,11 +72,11 @@ export default {
   created() {
     fetch(`${process.env.VUE_APP_SERVER_URL}/users/user/${this.form.userId}`)
       .then((res) => res.json())
-      .then((val) => (this.form = val));
+      .then((val) => (this.$store.state.user = {...val}));
   },
   data() {
     return {
-      form: this.$store.user,
+      form: { ...this.$store.state.user },
       show: true,
     };
   },
@@ -94,7 +84,7 @@ export default {
     onSubmit(event) {
       event.preventDefault();
       const option = {
-        method: "POST",
+        method: "PUT",
         mode: "cors",
         headers: {
           "Content-Type": "application/json",
@@ -104,22 +94,16 @@ export default {
 
       fetch(`${process.env.VUE_APP_SERVER_URL}/users/join`, option).then((res) => {
         if (res.ok) {
-          this.$router.push("/login");
+          this.$store.state.user = { ...this.form };
         } else {
-          alert("중복된 아이디 입니다");
+          alert("회원정보 수정 오류");
         }
       });
     },
     onReset(event) {
       event.preventDefault();
       // Reset our form values
-      this.form = {
-        userId: "",
-        userName: "",
-        userPassword: "",
-        address: "",
-        phoneNumber: "",
-      };
+      this.form = { ...this.$store.state.user };
       // Trick to reset/clear native browser form validation state
       this.show = false;
       this.$nextTick(() => {
