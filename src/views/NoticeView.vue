856c071f4dda3786ae1f2bd563924b483b8978f9
LeeHalim <leehalim09@gmail.com>
2022-11-08 17:59:58 +0900

    before impl notice

---
+++ src/views/NoticeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <MainHeaderVue></MainHeaderVue>
+    </div>
+</template>
+<script>
+import MainHeaderVue from "@/components/MainHeader.vue";
+
+export default {
+    components: {
+        MainHeaderVue
+    }
+}
+
+</script>
