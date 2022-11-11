0fb481272ffcda9987e8cc93e9bbbf28d1507a2d
LeeHalim <leehalim09@gmail.com>
2022-11-11 16:04:48 +0900

    resolved CORS using proxy

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-const axiosService = axios.create({
-    baseURL: process.env.VUE_APP_API_URL
-});
+const axiosService = axios.create();
 
 export default {
-    joinUser(userData) {
-        return axiosService.post('users/join', userData);
-    },
-    modifyUser(userData) {
-        return axiosService.put('users/join', userData);
-    },
-    loginUser(userData) {
-        return axiosService.post('users/login', userData);
-    },
-    getUser(userId) {
-        return axiosService.get(`users/user/${userId}`);
-    },
-    resignUser(userId) {
-        return axiosService.delete(`users/${userId}`);
-    },
-    getUserList() {
-        return axiosService.get('users/user-public');
-    }
-}
+  joinUser(userData) {
+    return axiosService.post("users/join", userData);
+  },
+  modifyUser(userData) {
+    return axiosService.put("users/join", userData);
+  },
+  loginUser(userData) {
+    return axiosService.post("users/login", userData);
+  },
+  getUser(userId) {
+    return axiosService.get(`users/user/${userId}`);
+  },
+  resignUser(userId) {
+    return axiosService.delete(`users/${userId}`);
+  },
+  getUserList() {
+    return axiosService.get("users/user-public");
+  },
+};
