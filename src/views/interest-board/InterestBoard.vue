d491ab83e3098b3a34640f0395e97d4c30900746
LeeHalim <leehalim09@gmail.com>
2022-11-24 08:54:57 +0900

    removed chart.js

--- src/views/interest-board/InterestBoard.vue
+++ src/views/interest-board/InterestBoard.vue
@@ -55,8 +55,6 @@
 import MainHeader from '@/components/MainHeader.vue';
 import KakaoMapVue from "@/components/KakaoMap.vue";
 import { estate as estateApi } from '@/api';
-import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
