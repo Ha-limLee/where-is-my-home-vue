f08335700defd75596d6a615ab1b874c46395d35
LeeHalim <leehalim09@gmail.com>
2023-01-27 00:52:41 +0900

    mocked user login using jwt

--- src/mocks/handle-api/auth.js
+++ src/mocks/handle-api/auth.js
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+import * as jose from 'jose';
 
 /**
  * @typedef {Object} User
@@ -7,6 +8,7 @@ import { rest } from "msw";
  * @property {string} [userName]
  * @property {string} [address]
  * @property {string} [phoneNumber]
+ * @property {'admin' | 'member'} role
  */
 
 /**
@@ -21,23 +23,22 @@ const userTable = {
     address: '서울특별시 서초구',
     phoneNumber: '010-1234-5678',
     userName: '김싸피',
+    role: 'admin',
   },
 };
+
+const secretKey = new TextEncoder().encode(')!+q90ije;;3vaeb1nu0e!5#z41');
+const alg = 'HS256';
 
 export default [
   rest.post('users/join', async (req, res, ctx) => {
     /** @type {User} */
     const body = await req.json();
-    console.log('여기 join');
-    console.log(body);
 
-    if (body.userId in userTable) {
-      return res(
-        ctx.status(409),
-      );
-    }
+    if (body.userId in userTable) return res( ctx.status(409) );
 
-    userTable[body.userId] = body;
+    userTable[body.userId] = { ...body, role: 'member' };
+
     return res(
       ctx.status(200),
     );
@@ -46,19 +47,38 @@ export default [
     /** @type {User} */
     const {userId, userPassword} = await req.json();
 
-    if (userId in userTable && userTable[userId].userPassword === userPassword) {
-      return res(
-        ctx.status(200),
-        ctx.set({
-          'access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxMjM0NTY3ODkwIiwiaWQiOiJzc2FmeSIsInJvbGUiOiJhZG1pbiIsInN1YiI6InNzYWZ5IiwidXNlcm5hbWUiOiLquYDsi7jtlLwifQ.qp68eVWlmpcKquF0hzPcMI1E1Jhe57BKmsC3s_qYAtY',
-          'refresh-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOiIxMjM0NTY3ODkwIiwiaWQiOiJzc2FmeSIsInJvbGUiOiJhZG1pbiIsInN1YiI6InNzYWZ5IiwidXNlcm5hbWUiOiLquYDsi7jtlLwifQ.qp68eVWlmpcKquF0hzPcMI1E1Jhe57BKmsC3s_qYAtY',
-        }),
-        ctx.json(),
-      )
-    }
+    if (!(userId in userTable) || userTable[userId].userPassword !== userPassword)
+      return res(ctx.status(200));
+
+    const user = userTable[userId];
+
+    const payload = {
+      exp: Date.now(),
+      id: user.userId,
+      role: user.role,
+      sub: user.userId,
+      username: user.userName ?? '',
+    };
+
+    const accessToken = await new jose.SignJWT(payload)
+      .setProtectedHeader({ alg, typ: "JWT" })
+      .setExpirationTime('30m')
+      .sign(secretKey);
+
+    const refreshToken = await new jose.SignJWT(payload)
+      .setProtectedHeader({ alg, typ: "JWT" })
+      .setExpirationTime('7d')
+      .sign(secretKey);
+
+    const tokens = {
+      'access-token': accessToken,
+      'refresh-token': refreshToken,
+    };
 
     return res(
       ctx.status(200),
+      ctx.set(tokens),
+      ctx.json(),
     );
   }),
 ];
