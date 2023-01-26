e058ffb7fd564dc4d56e14f891f7958796e51c54
LeeHalim <leehalim09@gmail.com>
2023-01-26 23:34:27 +0900

    mock user join

--- src/mocks/handle-api/auth.js
+++ src/mocks/handle-api/auth.js
@@ -4,14 +4,49 @@ import { rest } from "msw";
  * @typedef {Object} User
  * @property {string} userId
  * @property {string} userPassword
+ * @property {string} [userName]
+ * @property {string} [address]
+ * @property {string} [phoneNumber]
  */
 
+/**
+ * @typedef {Object<string, User>} UserTable
+ */
+
+/** @type {UserTable} */
+const userTable = {
+  'ssafy': {
+    userId: 'ssafy',
+    userPassword: 'ssafy',
+    address: '서울특별시 서초구',
+    phoneNumber: '010-1234-5678',
+    userName: '김싸피',
+  },
+};
+
 export default [
-  rest.post('/users/login', async (req, res, ctx) => {
+  rest.post('users/join', async (req, res, ctx) => {
     /** @type {User} */
     const body = await req.json();
+    console.log('여기 join');
+    console.log(body);
 
-    if (body.userId === 'ssafy' && body.userPassword === 'ssafy') {
+    if (body.userId in userTable) {
+      return res(
+        ctx.status(409),
+      );
+    }
+
+    userTable[body.userId] = body;
+    return res(
+      ctx.status(200),
+    );
+  }),
+  rest.post('/users/login', async (req, res, ctx) => {
+    /** @type {User} */
+    const {userId, userPassword} = await req.json();
+
+    if (userId in userTable && userTable[userId].userPassword === userPassword) {
       return res(
         ctx.status(200),
         ctx.set({
